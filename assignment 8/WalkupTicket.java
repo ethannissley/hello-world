59c0a55416b53c11ba9644c4a0d4c0a2eca9ead0
ethannissley <ethannissley96@gmail.com>
2018-03-05 12:33:42 -0800

    Create WalkupTicket.java

---
+++ assignment 8/WalkupTicket.java
@@ -0,0 +1,13 @@
+public class WalkupTicket extends Ticket {
+	//initializes walkup ticket with ticket number
+	WalkupTicket()
+	{
+		super();
+	}
+	
+	//returns the price of walkup ticket ($50)
+	public int getPrice()
+	{
+		return 50;
+	}
+}
