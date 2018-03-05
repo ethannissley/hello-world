a0235bbb7a63e705c05cccd66f1fd9f2efaeb43c
ethannissley <ethannissley96@gmail.com>
2018-03-05 12:08:12 -0800

    Create OctagonApp.java

---
+++ Assignment 9/OctagonApp.java
@@ -0,0 +1,18 @@
+public class OctagonApp implements Shape {
+	
+	private double side1;
+	
+    public OctagonApp(double side1) {
+    	this.side1 = side1;
+    	
+    }
+    // returns perimeter
+    public double getPerimeter() {
+    	return side1 * 8;
+    }
+    //returns area 
+    public double getArea() {
+    	//return  (2 + (4 / (Math.sqrt(2))) * side1 * side1); 
+    	return (2 + 4 / Math.sqrt(2)) * side1 * side1;
+    }
+}
