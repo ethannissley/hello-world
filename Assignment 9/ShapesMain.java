add8e2ee3a3434369228f1728b806bf7ba060de9
ethannissley <ethannissley96@gmail.com>
2018-03-05 12:12:52 -0800

    Update ShapesMain.java

--- Assignment 9/ShapesMain.java
+++ Assignment 9/ShapesMain.java
@@ -2,10 +2,9 @@ public class ShapesMain {
 
 	public static void main(String[] args) {
 		Shape[] shapes = new Shape[2];
-		shapes[0] = new OctagonApp(20);
+		shapes[0] = new Octagon(20);
 		shapes[1] = new HexagonApp(10);
 		
-	
 		for (int i = 0; i < shapes.length; i++ ) {
 			System.out.println("Area = " + shapes[i].getArea() + 
 			", perimeter = " + 
