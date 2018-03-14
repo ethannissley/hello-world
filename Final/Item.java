a4d8ea58d32632bc0a9246a925d779cf74c04440
ethannissley <ethannissley96@gmail.com>
2018-03-14 15:27:02 -0700

    Create Item.java

---
+++ Final/Item.java
@@ -0,0 +1,22 @@
+public abstract class Item {
+
+	public static void {
+	
+	}
+	
+	public static int checkIn() {
+		 
+		}
+	
+	public static int checkOut() {
+		  
+		}
+	
+	public static int payFines() {
+		
+		}
+	
+	public static int emailLibraryMember() {
+		   
+		}
+	
